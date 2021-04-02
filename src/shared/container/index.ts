4c08c6508f1c4297cf840c19853fc1c6fad85980
Lucas Rocha <lucas_rocha.14@outlook.com>
2021-04-02 11:08:34 -0300

    Creating useCase rental

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import {container} from 'tsyringe';
 
+import "@shared/container/providers";
+
 import { ICategoriesRepository } from '@modules/cars/repositories/ICategoriesRepository';
 import { ISpecificationRepository } from '@modules/cars/repositories/ISpecificationsRepository';
 import { IUsersRepository } from '@modules/accounts/repositories/IUsersRepository';
@@ -10,6 +12,8 @@ import { ICarsRepository } from '@modules/cars/repositories/ICarsRepository';
 import { CarsRepository } from '@modules/cars/infra/typeorm/repositories/CarsRepository';
 import { ICarsImagesRepository } from '@modules/cars/repositories/ICarsImagesRepository';
 import { CarsImagesRepository } from '@modules/cars/infra/typeorm/repositories/CarsImageRepository';
+import { IRentalsRepository } from '@modules/rentals/repositories/IRentalsRepository';
+import { RentalsRepository } from '@modules/rentals/infra/typeorm/repositories/RentalsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
     "CategoriesRepository",
@@ -29,3 +33,5 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<ICarsRepository>("CarsRepository", CarsRepository);
 
 container.registerSingleton<ICarsImagesRepository>("CarsImagesRepository", CarsImagesRepository);
+
+container.registerSingleton<IRentalsRepository>("RentalsRepository", RentalsRepository);
